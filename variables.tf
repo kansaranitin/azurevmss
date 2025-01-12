7483cc48e53946da87d5c2621e2ef76947d5e6ff
kansaranitin <82575173+kansaranitin@users.noreply.github.com>
2025-01-12 11:19:35 +0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,15 @@
 variable "client_id" {
-  description = "51a5d316-0f79-49bf-89a7-2559fb61145c"
+  description = "Azure Client ID"
 }
 
 variable "client_secret" {
-  description = "09f4fedf-e75c-413e-ae16-1d01b0729c4f"
+  description = "Azure Client Secret"
 }
 
 variable "tenant_id" {
-  description = "46c098eb-d201-4438-8d22-69c387d66aa8"
+  description = "Azure Tenant ID"
 }
 
 variable "subscription_id" {
-  description = "b1a16b61-64da-4d11-b76c-40be3788f709"
+  description = "Azure Subscription ID"
 }
